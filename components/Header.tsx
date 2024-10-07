fa0858cc113fbc7f64f56495ed0e73aabeb3d99c
Andrii Podolian <podolian.dev@outlook.com>
2024-10-07 17:18:31 +0300

    form

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,15 @@
+import Link from 'next/link'
+
 export default function Header() {
   return (
     <header className='bg-white shadow'>
       <div className='max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8'>
-        <h1 className='text-3xl font-bold text-gray-900'>Book Library</h1>
+        <div className='flex justify-between items-center'>
+          <h1 className='text-3xl font-bold text-gray-900'>Book Library</h1>
+          <Link href='/login' className='text-black-600 hover:text-blue-800'>
+            Login
+          </Link>
+        </div>
       </div>
     </header>
   )
